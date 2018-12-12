9009227b156f0502a229ec9417d1d13abbd27acf
revett <2796074+revett@users.noreply.github.com>
2018-12-12 20:43:41 +0000

    Update travis script

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Script that Travis CI will run to test the project, it is conditional on the branch.
 
-version=$(cat ./VERSION)
-
 set -e
 
 # Check that the VERSION file has been bumped on release branches
@@ -11,12 +9,13 @@ then
   make check-version
 fi
 
-diff_changes=$(go run ./ci/diff_changes.go -v)
-echo $diff_changes
+DIFF_CHANGES=$(go run ./ci/diff_changes.go)
+echo "Result of diff_changes.go: $DIFF_CHANGES"
 
-if [ "$diff_changes" = "both" ] || [ "$diff_changes" = "go" ]
+if [ "$DIFF_CHANGES" = "both" ] || [ "$DIFF_CHANGES" = "go" ]
 then 
   # Build and test the CLI
+  echo "Testing Go CLI"
   cd ./cli
   dep ensure -v
   make build
@@ -24,8 +23,9 @@ then
   cd ..
 fi
 
-if [ "$diff_changes" = "both" ] || [ "$diff_changes" = "other" ]
-then 
+if [ "$DIFF_CHANGES" = "both" ] || [ "$DIFF_CHANGES" = "other" ]
+then
   # Test the Docker Compose stack
+  echo "Testing Docker Compose stack"
   make integration-tests
 fi
