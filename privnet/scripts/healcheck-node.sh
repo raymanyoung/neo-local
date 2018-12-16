9cc8e8cf3cfbac1cbd3e4ae6d7f126d097e49baa
CryptoTuga <nuno@justoweb.com>
2018-12-16 15:46:46 +0000

    verbose messages

--- privnet/scripts/healcheck-node.sh
+++ privnet/scripts/healcheck-node.sh
@@ -3,7 +3,6 @@
 # This script tests if node is syncing correctly
 #
 #$NODE_NUMBER is passed as a evironment variable
-#set -e
 
 maxDelayBlocks=10
 actualRPCPort=`jq '.ApplicationConfiguration.RPC.Port' < /opt/node/neo-cli/config.json`
@@ -13,7 +12,7 @@ actualNodeBlock=`curl -s -X POST http://$actualNode -H 'Content-Type: applicatio
 if [ "$actualNodeBlock" == "" ]
 then
     echo "NODE IS DOWN"
-    exit 1
+    exit 503
 fi
 
 nodes=('neo-cli-privatenet-1:30333' 'neo-cli-privatenet-2:30334' 'neo-cli-privatenet-3:30335' 'neo-cli-privatenet-4:30336')
@@ -31,8 +30,8 @@ do
 
     if [ "$syncDelay" -gt "$maxDelayBlocks" ]
     then 
-        echo "NODE NOT SYNCED"
-        exit 1
+        echo "NODE OUT OF SYNC"
+        exit 408
     fi
 done
 
